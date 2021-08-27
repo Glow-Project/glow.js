70979df26d70ae8470c05df5217bc428fc76c285
Tch1b0 <Jpour3006@gmail.com>
2021-08-27 17:46:49 +0200

    Add `sendMessage` method

--- src/user/User.ts
+++ src/user/User.ts
@@ -1,9 +1,18 @@
+import { Message } from "../message";
+import { Session } from "../Session";
+
 export class User {
 	constructor(
 		public readonly name: string,
 		public readonly id: number,
 		public readonly rank: string
 	) {}
+
+	async sendMessage(text: string, session: Session) {
+		let msg = await Message.send(text, this.id, session);
+
+		return msg;
+	}
 
 	static fromObject(data: object) {
 		return new User(data["name"], data["id"], data["rank"]);
