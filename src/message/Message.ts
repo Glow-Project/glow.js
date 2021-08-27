3464dae65d6ddafa22a2d33941a9fc261593d8e7
Tch1b0 <Jpour3006@gmail.com>
2021-08-27 17:46:11 +0200

    Add send method

--- src/message/Message.ts
+++ src/message/Message.ts
@@ -1,3 +1,6 @@
+import FormData = require("form-data");
+import { Session } from "../Session";
+
 export class Message {
 	constructor(
 		public readonly id: number,
@@ -6,6 +9,16 @@ export class Message {
 		public readonly toId: number,
 		public readonly sentTimestamp: string
 	) {}
+
+	static async send(text: string, toId: number, session: Session) {
+		let form = new FormData();
+		form.append("to", toId);
+		form.append("body", text);
+
+		let msg = await session.post("message", form);
+
+		return msg;
+	}
 
 	static fromObject(data: object) {
 		return new Message(
